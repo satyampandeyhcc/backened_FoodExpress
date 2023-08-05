e5f51084ddafa4b080f9d6616e7918d964851eaa
satyampandeyhcc <satyampandeyhcc@gmail.com>
2023-08-05 18:20:26 +0530

    Modified

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 app.use(cors());
 app.use((req, res, next) => {
 
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
